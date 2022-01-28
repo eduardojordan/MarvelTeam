1e34ffd6b7c149ee07e5b3e91c6d08e6ae625b30
Eduardo Jordan <eduardojordan@hotmail.com>
2022-01-28 18:55:20 +0100

    add constants class

---
+++ MarvelTeam/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MarvelTeam
+//
+//  Created by Eduardo Jordan on 28/1/22.
+//
+
+import Foundation
+
+class Constants: NSObject {
+    static let API_KEY_PUBLIC = "30f82da5469dc0b00aab3559f26e2047"
+    static let API_KEY_PRIVATE = "5caaca8f42ab034276ef21802379e48072601df0"
+}
